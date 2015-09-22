240473b044e9250b5a8807c07929f57140dac169
imranariffin <imranariffin@yahoo.com>
2015-09-21 23:37:00 -0400

    surfaced aut/config.js for heroku push

--- auth/config.js
+++ auth/config.js
@@ -1,7 +1,7 @@
 // var callbackURL = path.join('http://imranariffin-login-pag.elasticbeanstalk.com', '/auth/facebook/callback');
 
+// production
 var productionCallbackUrl = 'http://my-sukan-2016.herokuapp.com/auth/facebook/callback';
-
 var mysukan_secret = '003b7b4ce78b7542a9da476995bedae2';
 var mysukan_id = '748112425316942';
 
