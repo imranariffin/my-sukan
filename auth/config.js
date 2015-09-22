3d8dd1e4a0f7a1d5bd02669cb57269bd401e6937
imranariffin <imranariffin@yahoo.com>
2015-09-21 23:29:20 -0400

    heroku push after configure new facebook app

--- auth/config.js
+++ auth/config.js
@@ -2,15 +2,20 @@
 
 var productionCallbackUrl = 'http://my-sukan-2016.herokuapp.com/auth/facebook/callback';
 
+var mysukan_secret = '003b7b4ce78b7542a9da476995bedae2';
+var mysukan_id = '748112425316942';
+
 var dummy;
 
 module.exports = {
 	'facebook' : {
-		'clientID' : '1620888751493470',
-		'clientSecret' : 'f376d18b026e5bb842062f86cde484ab',
+		// 'clientID' : '1620888751493470',
+		// 'clientSecret' : 'f376d18b026e5bb842062f86cde484ab',
 		// 'callbackURL' : callbackURL,
 		// 'callbackURL' : 'http://localhost:3000/auth/facebook/callback'
 		// 'callbackURL' : 'http://localhost:3000/auth/facebook/callback'
+		'clientID' : mysukan_id,
+		'clientSecret' : mysukan_secret,
 		'callbackURL' : productionCallbackUrl
 	}
 };
