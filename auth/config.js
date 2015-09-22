5bed07a9c48169c4dfdafd4442a3830dee211f01
imranariffin <imranariffin@yahoo.com>
2015-09-22 03:38:33 -0400

    facebook config

--- auth/config.js
+++ auth/config.js
@@ -9,13 +9,13 @@ var dummy;
 
 module.exports = {
 	'facebook' : {
-		'clientID' : '1620888751493470',
-		'clientSecret' : 'f376d18b026e5bb842062f86cde484ab',
+		// 'clientID' : '1620888751493470',
+		// 'clientSecret' : 'f376d18b026e5bb842062f86cde484ab',
 		// 'callbackURL' : callbackURL,
 		// 'callbackURL' : 'http://localhost:3000/auth/facebook/callback'
 		// 'callbackURL' : 'http://localhost:3000/auth/facebook/callback'
-		// 'clientID' : mysukan_id,
-		// 'clientSecret' : mysukan_secret
+		'clientID' : mysukan_id,
+		'clientSecret' : mysukan_secret
 		'callbackURL' : productionCallbackUrl
 	},
 	'twitter' : {
