c38c92a147fc380c252e33f0960bb74e0002f661
imranariffin <imranariffin@yahoo.com>
2015-09-22 03:32:26 -0400

    user production fb callback

--- auth/config.js
+++ auth/config.js
@@ -1,7 +1,7 @@
 // var callbackURL = path.join('http://imranariffin-login-pag.elasticbeanstalk.com', '/auth/facebook/callback');
 
 // production
-// var productionCallbackUrl = 'http://my-sukan-2016.herokuapp.com/auth/facebook/callback';
+var productionCallbackUrl = 'http://my-sukan-2016.herokuapp.com/auth/facebook/callback';
 var mysukan_secret = '003b7b4ce78b7542a9da476995bedae2';
 var mysukan_id = '748112425316942';
 
@@ -13,10 +13,10 @@ module.exports = {
 		'clientSecret' : 'f376d18b026e5bb842062f86cde484ab',
 		// 'callbackURL' : callbackURL,
 		// 'callbackURL' : 'http://localhost:3000/auth/facebook/callback'
-		'callbackURL' : 'http://localhost:3000/auth/facebook/callback'
+		// 'callbackURL' : 'http://localhost:3000/auth/facebook/callback'
 		// 'clientID' : mysukan_id,
 		// 'clientSecret' : mysukan_secret
-		// 'callbackURL' : productionCallbackUrl
+		'callbackURL' : productionCallbackUrl
 	},
 	'twitter' : {
 		'consumerKey' : 'VidKk5Rk8mCQswnS55gwvWFqU',
